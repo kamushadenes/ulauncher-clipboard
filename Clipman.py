3e82396890d9b99f976c7338aae0005f79519a07
Kamus Hadenes <kamus@hadenes.io>
2021-01-13 06:16:34 -0300

    add clipman support

---
+++ Clipman.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+from lib import logger, execGet, findExec, pidOf
+
+
+name = "Clipman"
+client = "clipman"
+
+
+def canStart():
+    return bool(findExec(client))
+
+
+def isRunning():
+    return bool(pidOf("clipman"))
+
+
+def isEnabled():
+    return canStart() and isRunning()
+
+
+def start():
+    pass
+    # subprocess.call([client, 'start'])
+
+
+def add(text):
+    pass
+    # subprocess.call([client, 'add', text])
+
+
+def getHistory():
+    # The only separator options are zero bytes and line breaks.
+    # Line breaks are very likely to be in the actual clipboard entries, so we can't use that.
+    # Zero bytes are less likely, but would not be my first choice.
+    return execGet("sh", "-c", "{0} pick --tool=STDOUT --print0".format(client)).split(
+        "\x00"
+    )
